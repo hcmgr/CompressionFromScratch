6422134760b7d97b708d7f5b4992ce13a9a3a320
Harry Rackemann <harryrackemann@gmail.com>
2024-07-05 15:25:49 +1000

    implemented rle encoding

--- comp.cpp
+++ comp.cpp
@@ -17,8 +17,22 @@ void display_image(cv::Mat& image, std::string name) {
 /**
  * Run-length-encoding of given block array
  */
-int rle(std::vector<uchar>) {
-    return 0;
+std::vector<uchar> rle(std::vector<uchar> block_array) {
+    std::vector<uchar> rle_array;
+
+    int n = block_array.size();
+    int i = 0, j = 0;
+    while (i < n) {
+        j = i+1;
+        while (block_array[j] == block_array[i]) {
+            j++;
+        }
+        rle_array.push_back((j - 1)); // count
+        rle_array.push_back((block_array[i])); // number
+        i = j;
+    }
+
+    return rle_array;
 }
 
 int muckin() {
@@ -38,9 +52,8 @@ int muckin() {
     cv::Mat blue_channel = channels[1];
 
     // extracting 8x8 block
-    cv::Rect roi_rect(0, 0, 8, 8);
+    cv::Rect roi_rect(150, 150, 8, 8);
     cv::Mat block = blue_channel(roi_rect).clone();
-    std::cout << block << std::endl;
 
     // flatten and convert to vector<uchar>
     cv::Mat flattenedBlock = block.reshape(0, 1).clone();
@@ -49,9 +62,14 @@ int muckin() {
     for (uchar el : flattenedVec) {
         std::cout << static_cast<int>(el) << std::endl;
     }
-    std::cout << flattenedVec.size() << std::endl;
+    std::cout << "-------------" << std::endl;
 
-    display_image(image, image_name);
+    // rle encode vector
+    std::vector<uchar> rleVec = rle(flattenedVec);
+    for (uchar el : rleVec) {
+        std::cout << static_cast<int>(el) << std::endl;
+    }
+
     return 0;
 }
 
